1b593eb87d671a2cc37252651651a2e87987f3c6
Aditya Sinha <adityasinha361@gmail.com>
2023-01-20 00:29:19 +0530

    chore: add test cases for calculating the maximum bowling score

--- test/bowlingGameUtility.test.js
+++ test/bowlingGameUtility.test.js
@@ -1,4 +1,4 @@
-const { calculateBowlingScore } = require('../src/bowlingGameUtility');
+const { calculateBowlingScore, findMaximumScore } = require('../src/bowlingGameUtility');
 describe('Bowling Game Utility', () => {
 
   describe('Bowling game score calculator', () => {
@@ -23,5 +23,26 @@ describe('Bowling Game Utility', () => {
       expect(calculateBowlingScore([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])).toBe(300);
     });
 
+    it('Should return total score of a game when input is correct', () => {
+      expect(calculateBowlingScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+    });
+
+    it('Should return total score of a game when input is correct', () => {
+      expect(calculateBowlingScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
+    });
+  });
+
+  describe('Maximum bowling score calculator', () => {
+    it('Should return maximum score of multiple bowling games', () => {
+      expect(findMaximumScore([[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]])).toBe(300);
+    });
+
+    it('Should return maximum score of multiple bowling games', () => {
+      expect(findMaximumScore([[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]])).toBe(30);
+    });
+
+    it('Should return maximum score of multiple bowling games', () => {
+      expect(findMaximumScore([[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]])).toBe(90);
+    });
   });
 });
